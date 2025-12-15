1fe0e55ac62f4c54acb19301f99bddfa313cd5ba
Emre-Taro <emre.t.hatanaka@gmail.com>
2025-12-15 16:42:19 +0900

    First commit - basic setting to start playwright

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [{ name: 'chromium', use: { browserName: 'chromium' } }],
+});
